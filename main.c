140afa2d0785cad3f50c51f59f0511af19819485
blissmal <malutibethuel@gmail.com>
2023-11-23 15:47:23 +0300

    updated

--- main.c
+++ main.c
@@ -3,14 +3,29 @@
 int main() {
 
     /// We can use a 2d array to perform matrix multiplication
+    printf("Enter the values for the matrix\n");
+    printf("\t\tA\tB\t\t\tE\tF\n");
+    printf("\t\tC\tD\t\t\tG\tH\n");
+    int vals[8];
+
+    int List[8] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+    for (int i = 0; i < 8; ++i) {
+        printf("Enter value for %c: ", List[i]);
+        scanf("%d", &vals[i]);
+    }
+    printf("Multiplication of: \n"
+           "\t\t%d\t%d\t\t%d\t%d\n\t\t%d\t%d\t\t%d\t%d\n",
+           vals[0], vals[1], vals[4], vals[5], vals[2], vals[3], vals[6], vals[7]);
+
     int matrixA[2][2] = {
-            2,2,
-            2,2
+            vals[0],vals[1],
+            vals[2],vals[3]
     };
 
     int matrixB[2][2] = {
-            3,4,
-            3,6
+            vals[4],vals[5],
+            vals[6],vals[7]
     };
 
     int col1 = (matrixA[0][0] * matrixB[0][0]) + (matrixA[0][1] * matrixB[1][0]);
@@ -22,10 +37,12 @@ int main() {
             col1, col2,
             col3, col4
     };
+    printf("\tTHE MULTIPLICATION OF MATRIX 1 AND MATRIX 2 IS: ");
+    printf("\n");
 
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            printf("%d\t", NewMat[i][j]);
+            printf("\t\t%d\t", NewMat[i][j]);
         }
         printf("\n");
     }
@@ -33,5 +50,5 @@ int main() {
 
 
     return 0;
-    /// PREPARED BY BLISSMAL
+    /// ------------------------PREPARED BY BLISSMAL
 }
